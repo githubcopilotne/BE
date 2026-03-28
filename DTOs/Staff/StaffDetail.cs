ece5a3472698b6e7d3fb593b572811eff9a25924
githubcopilot <maildemothoil@gmail.com>
2026-03-28 18:19:11 +0700

    response 3 field for staff

--- DTOs/Staff/StaffDetail.cs
+++ DTOs/Staff/StaffDetail.cs
@@ -10,6 +10,9 @@ namespace BE.DTOs.Staff
         public int? Gender { get; set; }
         public DateOnly? Birthday { get; set; }
         public string? Address { get; set; }
+        public string? ProvinceName { get; set; }
+        public string? DistrictName { get; set; }
+        public string? WardName { get; set; }
         public string Role { get; set; } = null!;
         public string? IdCard { get; set; }
         public DateOnly? HireDate { get; set; }
